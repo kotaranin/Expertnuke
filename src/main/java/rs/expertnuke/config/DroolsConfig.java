0caa01414dfc43433427e76c9b7e4aad41deb39a
kotaranin <kotaraninuros@gmail.com>
2026-06-21 14:06:15 +0200

    DroolsConfig simplified.

--- src/main/java/rs/expertnuke/config/DroolsConfig.java
+++ src/main/java/rs/expertnuke/config/DroolsConfig.java
@@ -1,7 +1,6 @@
 package rs.expertnuke.config;
 
 import org.kie.api.KieServices;
-import org.kie.api.builder.KieBuilder;
 import org.kie.api.builder.KieFileSystem;
 import org.kie.api.runtime.KieContainer;
 import org.springframework.context.annotation.Bean;
@@ -13,13 +12,10 @@ public class DroolsConfig {
     
     @Bean
     public KieContainer getKieContainer() {
-        System.setProperty("drools.programmatic.model", "false");
         KieServices ks = KieServices.Factory.get();
         KieFileSystem kfs = ks.newKieFileSystem();
-        kfs.write("src/main/resources/rs/expertnuke/rules/reactor-rules.drl", 
-                  ResourceFactory.newClassPathResource("rules/reactor-rules.drl"));
-        KieBuilder kb = ks.newKieBuilder(kfs);
-        kb.buildAll(org.drools.compiler.kie.builder.impl.DrlProject.class);
+        kfs.write(ResourceFactory.newClassPathResource("rules/reactor-rules.drl"));
+        ks.newKieBuilder(kfs).buildAll();
         return ks.newKieContainer(ks.getRepository().getDefaultReleaseId());
     }
 }
